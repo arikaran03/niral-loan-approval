80b2298af2eb0d8ef1b726ebea635c211bdfcf65
arikaran03 <arikaran258@gmail.com>
2025-05-10 21:25:38 +0530

    added Email sender function ->  sendConfiguredEmail into communicate.js

--- server/config.js
+++ server/config.js
@@ -8,10 +8,20 @@ const JWT_SECRET = process.env.JWT_SECRET;
 const CLIENT_URL = process.env.CLIENT_URL;
 const DB_NAME = process.env.DB_NAME;
 
+// Email Sending configuration
+const SENDER_EMAIL = process.env.SENDER_EMAIL;
+const SENDER_PASSWORD = process.env.SENDER_PASSWORD;
+const SMTP_SERVER = process.env.SMTP_SERVER; // smtp.gmail.com 
+const SMTP_PORT = parseInt(process.env.SMTP_PORT || "587", 10); // 585 or 645
+
 export {
     MONGODB_URL,
     PORT,
     JWT_SECRET,
     CLIENT_URL,
-    DB_NAME
+    DB_NAME,
+    SENDER_EMAIL,
+    SENDER_PASSWORD,
+    SMTP_SERVER,
+    SMTP_PORT
 }
